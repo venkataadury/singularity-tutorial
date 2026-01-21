9d32e593083d49b56a80b37e61b0ac566878503e
Venkata Sai Sreyas Adury <venkata.adury0307@gmail.com>
2026-01-21 08:06:39 -0500

    Docker to SIF

--- containers/build_from_docker/docker_to_sif.sh
+++ containers/build_from_docker/docker_to_sif.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 #
-# Remember the tag used for docker image (af3_docker by default)
-# 1. Save the docker container to a local tar file
-sudo docker save af3_docker:latest -o af3_docker.tar
-
-# 2. Use local tar file to make singularity image
-sudo singularity build alphafold3_singularity.sif af3_docker.tar
-
-# 3. Cleanup - Delete the temporary tar file
-sudo rm -f af3_docker.tar 
+sudo singularity build alphafold3_singularity.sif docker-daemon://af3_docker:latest
